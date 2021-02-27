aa2f5acefcb8d79e50d7013986295ca214f80283
Peter Ayeni <ayenicology@gmail.com>
2021-02-27 21:56:03 +0000

    feat: add create product form

--- components/CreateProduct.js
+++ components/CreateProduct.js
@@ -1,22 +1,58 @@
 import useForm from '../lib/useForm';
+import Form from './styles/Form';
 
 export default function CreateProduct() {
-  const { inputs, handleChange, clearForm, resetForm } = useForm({});
+  const { inputs, handleChange, clearForm, resetForm } = useForm({
+    image: '',
+    name: 'Nice Shoe',
+    price: 3523,
+    description: 'These is the best shoes for winter',
+  });
   return (
-    <form>
-      <label htmlFor="name">
-        Name
-        <input
-          name="name"
-          type="text"
-          id="name"
-          value={inputs.name}
-          onChange={handleChange}
-        />
-      </label>
-      <button type="button" onClick={clearForm}>
-        Clear Form
-      </button>
-    </form>
+    <Form
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+    >
+      <fieldset>
+        <label htmlFor="image">
+          Image
+          <input name="name" type="file" id="image" onChange={handleChange} />
+        </label>
+        <label htmlFor="name">
+          Name
+          <input
+            name="name"
+            type="text"
+            id="name"
+            placeholder="Name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            name="price"
+            type="number"
+            id="price"
+            placeholder="Price"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <textarea
+            name="description"
+            id="description"
+            placeholder="Description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">+ Add Product</button>
+      </fieldset>
+    </Form>
   );
 }
